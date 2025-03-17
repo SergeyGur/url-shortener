71bf2939635bd97eee8257109b514ff747c11cdc
SegreyGur <gurjanov.sergey@icloud.com>
2025-03-17 16:06:54 +0300

    add sqlite

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -4,6 +4,8 @@ import (
 	"log/slog"
 	"os"
 	"url-shortener/internal/config"
+	"url-shortener/internal/lib/logger/sl"
+	"url-shortener/internal/storage/sqlite"
 )
 
 const (
@@ -14,9 +16,18 @@ const (
 
 func main() {
 	cfg := config.MustLoad()
+
 	log := setupLogger(cfg.Env)
+
 	log.Info("starting url-shortener", slog.String("env", cfg.Env))
 	log.Debug("debug message are enabled")
+
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+	_ = storage
 }
 
 func setupLogger(env string) *slog.Logger {
